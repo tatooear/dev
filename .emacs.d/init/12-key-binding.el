75e156f041dbc48d596af9f80263c571b694457b
ta28 <you@example.com>
2019-11-24 22:22:25 +0900

    emacs update

--- .emacs.d/init/12-key-binding.el
+++ .emacs.d/init/12-key-binding.el
@@ -14,7 +14,40 @@
 (define-key function-key-map [apps] 'event-apply-hyper-modifier)        ;; super キーの設定
 (define-key function-key-map [lwindow] 'event-apply-super-modifier) ;; super キーの設定
 
+(global-set-key "\C-z" nil)
+
+(global-set-key "\C-n" 'next-line-recenter)
+(global-set-key "\C-p" 'previous-line-recenter)
+(global-set-key "\C-h" 'delete-backward-char)                 ;; 文字の削除
+(global-set-key "\C-w" 'my-kill-region-or-backward-kill-word) ;; 単語を削除
+(global-set-key "\C-\\" 'redo) ;; やり直し
+(global-set-key (kbd "C-}") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-{") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-*") 'mc/mark-all-dwim)
+(global-set-key (kbd "C-^") popwin:keymap)
+(global-set-key (kbd "C-+") 'increment-string-as-number)
+(global-set-key (kbd "C--") 'decrement-string-as-number)
+(global-set-key (kbd "C-;") 'switch-to-previous-buffer)
 (global-set-key (kbd "C-,") 'tabbar-backward-tab)
 (global-set-key (kbd "C-.") 'tabbar-forward-tab)
 
-(global-set-key "\C-z" nil)
+(global-set-key "\C-xri"   'insert-pair-region) ;;regionを文字列で囲む
+(global-set-key "\C-xrr"   'replace-rectangle) ;;矩形分の文字列を置換
+(global-set-key "\C-xrn"   'number-rectangle) ;;矩形の左端に数列を挿入
+
+(global-set-key "\C-z\C-f" 'counsel-find-file)
+(global-set-key "\C-z\C-g" 'counsel-ag)
+(global-set-key "\C-z\C-i" 'counsel-imenu)
+(global-set-key "\C-z\C-r" 'counsel-recentf)
+
+(global-set-key "\M-gg" 'nil)
+(global-set-key "\M-g\M-g" 'nil)
+(global-set-key "\M-g" 'goto-line)
+(global-set-key (kbd "M-n") 'next-line-visual)
+(global-set-key (kbd "M-p") 'previous-line-visual)
+
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "M-y") 'counsel-yank-pop)
+(global-set-key (kbd "M-w") 'my-kill-ring-save-or-backward-kill-line) ;; 行頭まで削除
+(global-set-key (kbd "M-z") 'zop-up-to-char)
+
